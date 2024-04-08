68e9a88de069183506c6f53a265b81941ecc10ac
Emilse Arias <emilsearias@gmail.com>
2024-04-08 20:43:01 -0300

    agregamos el componente Informacion.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
- 
   return (
-    <h1>Prueba de titulo</h1>
-  )
+    <main className="container my-5">
+      <h1>Primer proyecto con React</h1>
+    
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Informacion.jsx
@@ -0,0 +1,17 @@
+const Informacion = () => {
+    return (
+        <>
+         <h2>Conceptos de react</h2>
+         <p>React es una biblioteca de JavaScript desarrollada por Facebook que se
+          utiliza para construir interfaces de usuario interactivas y
+          eficientes. Su enfoque se centra en la creación de componentes
+          reutilizables que representan diferentes partes de la interfaz de
+          usuario. React utiliza un paradigma de programación declarativa, lo
+          que significa que los desarrolladores describen cómo debería ser la
+          interfaz de usuario y React se encarga de manejar los cambios de
+          manera eficiente.</p>   
+        </>
+    );
+};
+
+export default Informacion;
