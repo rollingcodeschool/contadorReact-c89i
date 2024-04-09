459407d3e9b613bc107b164e5ec72f49724d9cc2
Emilse Arias <emilsearias@gmail.com>
2024-04-08 21:11:18 -0300

    enviamos un props al componete información

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Informacion from "./components/Informacion";
+
 
 function App() {
   return (
     <main className="container my-5">
       <h1>Primer proyecto con React</h1>
-    
+      <Informacion comision='c89i'></Informacion>
+      {/* <Informacion /> */}
     </main>
   );
 }
--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,4 +1,4 @@
-const Informacion = () => {
+const Informacion = (props) => {
     return (
         <>
          <h2>Conceptos de react</h2>
@@ -9,7 +9,7 @@ const Informacion = () => {
           usuario. React utiliza un paradigma de programación declarativa, lo
           que significa que los desarrolladores describen cómo debería ser la
           interfaz de usuario y React se encarga de manejar los cambios de
-          manera eficiente.</p>   
+          manera eficiente.<br/> Comision: {props.comision}</p>   
         </>
     );
 };
